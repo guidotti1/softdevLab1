532d1aa8b9a868db91621e2100a7e6ca75e5a9f6
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-09-19 15:50:05 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,7 @@ int main()
 {
     //main
     //passes input file name to dataLookup constructor, constructs the map indexed by PID
-    string fileName = "/home/class/SoftDev/marvel/marvel-wikia-data";
+    string fileName = "/home/class/SoftDev/marvel/marvel-wikia-data.csv";
     dataLookup ourMap(fileName);
     //create year and name maps used for user lookup
     ourMap.createYearAndNameMaps();
