54d8c96bc33e52147437007107337b0776519644
guidotti1 <54955400+guidotti1@users.noreply.github.com>
2019-09-29 12:51:42 -0400

    Update marvelLookup.cpp

--- marvelLookup.cpp
+++ marvelLookup.cpp
@@ -28,29 +28,33 @@ int main()
     //gets info from web page
     form_iterator itts = cgi.getElement("type_select");
     form_iterator itname = cgi.getElement("name");
+    form_iterator itcharNumber = cgi.getElement("charNumber");
     //gets the parameters from web page
     string type = **itts;
+    char ctype = type[0];
     string name = **itname;
+    string charNumber = **itcharNumber;
+    int charNumInt;
+    istringstream iss(charNumber);
+    iss >> charNumInt;
     //passes input file name to dataLookup constructor, constructs the map indexed by PID
-    string fileName = "input.txt";
+    string fileName = "guidotti1_input.txt";
     dataLookup ourMap(fileName);
     //create year and name maps used for user lookup
     ourMap.createYearAndNameMaps();
-    //allows user to repeat
-    //bool repeat = true;
-    //while (repeat)
-        //{
-        vector<dataEntry> characters;
-        characters = ourMap.userSearch(type, name);
+    
+    vector<dataEntry> characters;
+    //we read matches for a name/year if the user hasn't selected a specific character,i.e. we haven't read matches
+    characters = ourMap.userSearch(ctype, name);
+    if (charNumInt == 0)
+    {
         ourMap.readMatches(characters);
-        //cout << "Enter 0 and press return to quit. Enter any other character to search again" << endl;
-        //char repeatChar;
-        //cin >> repeatChar;
-        //if (repeatChar == '0')
-           // {
-           // repeat = false;
-           // }
-        //}
+    }
+    else
+    {
+        cout<<characters[charNumInt-1];
+    }
+
 return 0;
 }
 
